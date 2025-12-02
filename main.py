537a8fea12dd56cc707af959a7661632026a053a
gibgichuki-a <gibgichuki@gmail.com>
2025-12-02 22:21:23 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from utils import maximum
+x= int(input("Enter the first number: "))
+y= int(input("Enter the second number: "))
+print(f"the maximum number between {x} and {y} is {maximum(x,y)}")
+
+
+
+
+
+
